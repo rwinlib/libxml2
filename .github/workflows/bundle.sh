5ddf038959c6e45d936d9dbf945327b65811c3a5
Jeroen Ooms <jeroenooms@gmail.com>
2023-02-21 14:14:18 +0100

    add CI

---
+++ .github/workflows/bundle.sh
@@ -0,0 +1,36 @@
+#/bin/sh
+set -e
+PACKAGE=libxml2
+
+# Update
+pacman -Syy --noconfirm
+OUTPUT=$(mktemp -d)
+
+# Download files (-dd skips dependencies)
+pkgs=$(echo mingw-w64-{i686,x86_64,ucrt-x86_64}-{libxml2,libxslt,xz})
+URLS=$(pacman -Spdd $pkgs --cache=$OUTPUT)
+VERSION=$(pacman -Si mingw-w64-x86_64-${PACKAGE} | awk '/^Version/{print $3}')
+
+# Set version for next step
+echo "::set-output name=VERSION::${VERSION}"
+echo "::set-output name=PACKAGE::${PACKAGE}"
+echo "Bundling $PACKAGE-$VERSION"
+echo "# $PACKAGE $VERSION" > README.md
+echo "" >> README.md
+
+for URL in $URLS; do
+  curl -OLs $URL
+  FILE=$(basename $URL)
+  echo "Extracting: $URL"
+  echo " - $FILE" >> README.md
+  tar xf $FILE -C ${OUTPUT}
+  rm -f $FILE
+done
+
+# Copy libs
+rm -Rf include lib*
+mkdir -p lib/x64 lib-8.3.0/{x64,i386}
+cp -v ${OUTPUT}/ucrt64/lib/*.a lib/x64/
+cp -v ${OUTPUT}/mingw64/lib/*.a lib-8.3.0/x64/
+cp -v ${OUTPUT}/mingw32/lib/*.a lib-8.3.0/i386/
+cp -rv ${OUTPUT}/ucrt64/include .
--- readme.md
+++
@@ -1,8 +0,0 @@
-# libxml2
-
-Built with rtools40:
-
- - libxml2: 2.9.10
- - libxslt: 1.1.34
- 
-Client must build with `-DLIBXML_STATIC` to link against static libraries.
